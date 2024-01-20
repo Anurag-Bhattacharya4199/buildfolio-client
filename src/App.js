315149f837e43e958a6d6c16838fffae7f26eb7f
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2024-01-20 15:31:43 -0500

    Working progress of client app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main/Main";
+import Header from "./components/Header/Header";
+import AboutProject from "./pages/AboutProject/AboutProject";
+import Footer from "./components/Footer/Footer";
+import PorfolioForm from "./pages/PortfolioForm/PortfolioForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/aboutProject" element={<AboutProject />} />
+          <Route path="/buildPortfolio" element={<PorfolioForm />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
