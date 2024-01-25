79737b34b739475fc42d6ed15f1a08178e4cd138
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2024-01-24 21:49:58 -0500

    Posting of User + Getting of User in progress

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./pages/Main/Main";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import UserForm from "./pages/UserForm/UserForm";
+import UserDashboard from "./pages/UserDashboard/UserDashboard";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/buildUser" element={<UserForm />} />
+          <Route path="/user/:id" element={<UserDashboard />} />
         </Routes>
         <Footer />
       </BrowserRouter>
