59dec969a8664eb7cd62539be5012ef8420345bf
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2024-01-27 09:45:50 -0500

    User Form Full Validation + User Dashboard Styling Completed

--- src/pages/UserDashboard/UserDashboard.js
+++ src/pages/UserDashboard/UserDashboard.js
@@ -26,25 +26,47 @@ function UserDashboard() {
     return null;
   } else {
     return (
-      <>
-        <h1 className={`${user.user_primaryColor}`}>
-          {user.user_name}'s Profile:
-        </h1>
-        <h2 className={`${user.user_secondaryColor}`}>Email Address</h2>
-        <p>{user.user_email}</p>
-        <h2 className={`${user.user_secondaryColor}`}>Phone Number</h2>
-        <p>{user.user_phoneNum}</p>
-        <h2 className={`${user.user_secondaryColor}`}>Summary:</h2>
-        <p>{user.user_summary}</p>
-        <h2 className={`${user.user_secondaryColor}`}>LinkedIn:</h2>
-        <a href={`${user.user_linkedIn}`} target="_blank" rel="noreferrer">
-          <img src={LinkedInIcon} alt="LinkedIn Logo" />
-        </a>
-        <h2 className={`${user.user_secondaryColor}`}>GitHub:</h2>
-        <a href={`${user.user_github}`} target="_blank" rel="noreferrer">
-          <img src={GitHubIcon} alt="LinkedIn Logo" />
-        </a>
-      </>
+      <section className="userDashboard">
+        <div className="userDashboard__mainTitle">
+          <h1 className={`${user.user_primaryColor}`}>
+            {user.user_name}'s Profile:
+          </h1>
+        </div>
+        <div className="userDashboard__contacts">
+          <h2 className={`${user.user_secondaryColor}`}>Email Address</h2>
+          <p>{user.user_email}</p>
+          <h2 className={`${user.user_secondaryColor}`}>Phone Number</h2>
+          <p>{user.user_phoneNum}</p>
+        </div>
+        <div className="userDashboard__summary">
+          <h2 className={`${user.user_secondaryColor}`}>Summary:</h2>
+          <p>{user.user_summary}</p>
+        </div>
+        <div className="userDashboard__socialAccounts">
+          <h2 className={`${user.user_secondaryColor}`}>
+            {user.user_name}'s LinkedIn:
+          </h2>
+          <a href={`${user.user_linkedIn}`} target="_blank" rel="noreferrer">
+            <img src={LinkedInIcon} alt="LinkedIn Logo" />
+          </a>
+          <h2 className={`${user.user_secondaryColor}`}>
+            {user.user_name}'s GitHub:
+          </h2>
+          <a href={`${user.user_github}`} target="_blank" rel="noreferrer">
+            <img src={GitHubIcon} alt="LinkedIn Logo" />
+          </a>
+        </div>
+        <div className="userDashboard__buttons">
+          <button>Add Education</button>
+          <button>Add Professional History</button>
+          <button>Add Projects</button>
+          <button>Add Skills</button>
+          <button>Add References</button>
+        </div>
+        <div>
+          <button>BUILD MY PORTFOLIO</button>
+        </div>
+      </section>
     );
   }
 }
